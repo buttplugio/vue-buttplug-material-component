cb27b422767e2268d062957a5ce549d9eda212cc
Kyle Machulis <kyle@nonpolynomial.com>
2017-10-08 22:56:46 -0700

    build: Build release files for 0.0.2 release

--- dist/types/ButtplugMessageBus.d.ts
+++ dist/types/ButtplugMessageBus.d.ts
@@ -1,4 +1,5 @@
 import Vue from "vue";
 import { ButtplugMessage, Device } from "buttplug";
 export declare const ButtplugMessageBus: Vue;
-export declare function SendMessageToDevice(aDevice: Device, aMessage: ButtplugMessage): void;
+export declare function SendDeviceMessage(aDevice: Device, aMessage: ButtplugMessage): void;
+export declare function StopAllDevices(): void;
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,8 +1,7 @@
 import Vue from "vue";
 import ButtplugPanelType from "./ButtplugPanel/ButtplugPanel";
 import ButtplugPanelComponent from "./ButtplugPanel/ButtplugPanel.vue";
-import { SendMessageToDevice } from "./ButtplugMessageBus";
-export { ButtplugPanelComponent, ButtplugPanelType, SendMessageToDevice };
+export { ButtplugPanelComponent, ButtplugPanelType };
 export declare function install(vue: typeof Vue, options?: {
     prefix: string;
 }): void;
