f32526e5e4804ac9aeb629e76f48eccd7cfa118b
Kyle Machulis <kyle@nonpolynomial.com>
2017-11-26 19:58:57 -0800

    build: Actually build 0.0.4 artifacts now

--- dist/types/ButtplugMessageBus.d.ts
+++ dist/types/ButtplugMessageBus.d.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
 import { ButtplugMessage, Device } from "buttplug";
-export declare const ButtplugMessageBus: Vue;
+export declare const ButtplugMessageBus: object & Record<never, any> & Vue;
 export declare function SendDeviceMessage(aDevice: Device, aMessage: ButtplugMessage): void;
 export declare function StopAllDevices(): void;
