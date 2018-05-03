cfddccae29f5048fc3cc74c199ac671fd3d5bf75
Kyle Machulis <kyle@nonpolynomial.com>
2018-05-03 11:28:17 -0700

    build: Release artifacts for 0.2.1

--- dist/ButtplugConnectionManager/ButtplugStartConnectEvent.js
+++
@@ -1,7 +0,0 @@
-export default class ButtplugStartConnectEvent {
-    constructor(clientName, address) {
-        this.clientName = clientName;
-        this.address = address;
-    }
-}
-//# sourceMappingURL=ButtplugStartConnectEvent.js.map
--- dist/ButtplugMessageBus.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-export const ButtplugMessageBus = new Vue();
-export function SendDeviceMessage(aDevice, aMessage) {
-    ButtplugMessageBus.$emit("devicemessage", aDevice, aMessage);
-}
-export function StopAllDevices() {
-    ButtplugMessageBus.$emit("stopalldevices");
-}
-//# sourceMappingURL=ButtplugMessageBus.js.map
